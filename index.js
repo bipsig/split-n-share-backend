b5797bf7369d3711647f4c65d8f00af7743b2141
bipsig <sagnikd.ug20.me@nitp.ac.in>
2025-01-03 22:59:23 +0530

    [MJS-37] Setup MongoDB Atlas server
    -- Setup a new cluster in the MongoDB Atlas account
    -- Added the username and password as enviroment variables
    -- Installed npm package dotenv to use the environment variables
    -- Made some dummy calls to check connection

--- index.js
+++ index.js
@@ -1,9 +1,41 @@
 import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import { MongoClient } from "mongodb"
+
+dotenv.config();
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
+const mongo_uri = `mongodb+srv://${process.env.MONGODB_ATLAS_USERNAME}:${process.env.MONGODB_ATLAS_PASSWORD}@split-n-share.e8f54.mongodb.net/?retryWrites=true&w=majority&appName=split-n-share`;
 
-app.get ('/', (req, res) => {
+const connectMongoDB = async () => {
+    const client = new MongoClient (mongo_uri);
+
+    try {
+
+        await client.connect();
+        console.log('Connected to MongoDB Atlas');
+    
+        const database = client.db('testDatabase');
+        const collection = database.collection('testCollection');
+    
+        const doc = { name: 'Sagnik', age: 22, city: 'Pune' };
+        const result = await collection.insertOne(doc);
+        console.log('Document inserted with _id:', result.insertedId);
+    
+        const documents = await collection.find({}).toArray();
+        console.log('Documents:', documents);
+    
+      } 
+        
+      catch (error) {
+        console.error('Error connecting to MongoDB:', error);
+    }
+}
+
+app.get ('/', async (req, res) => {
+    connectMongoDB();
     res.send ('Hello World!');
 })
 
